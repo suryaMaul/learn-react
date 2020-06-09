ab37f3cfffde97737949e8bdcb0c7bb99807fb2b
surya maulana <suryamaulana1980@gmail.com>
2020-06-09 11:14:27 +0700

    fix bab 20

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -11,7 +11,8 @@ import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 import * as burgerBuilderActions from '../../store/actions/index';
 import axios from '../../axios-orders';
 
-class BurgerBuilder extends Component{
+export class BurgerBuilder extends Component{
+// class BurgerBuilder extends Component{
     // constructor(props) {
     //     super(props);
     //     this.state = {...}
